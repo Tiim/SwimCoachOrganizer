7bd6677e8c16a843e8711e600878f2746e59a9e0
Tiim <tim.bachmann@cad.ch>
2016-01-25 00:36:13 +0100

    Add prototype of the about dialog.

--- src/main/java/ch/tiim/sco/gui/MainWindow.java
+++ src/main/java/ch/tiim/sco/gui/MainWindow.java
@@ -3,10 +3,12 @@ package ch.tiim.sco.gui;
 import ch.tiim.inject.Inject;
 import ch.tiim.sco.config.Config;
 import ch.tiim.sco.gui.dialog.DialogView;
+import ch.tiim.sco.gui.events.AboutEvent;
 import ch.tiim.sco.gui.main.*;
 import ch.tiim.sco.gui.util.DialogListener;
 import ch.tiim.sco.update.Version;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -16,6 +18,9 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -90,12 +95,29 @@ public class MainWindow extends View {
                 });
             }
         }
+        toolBar.getItems().addAll(getSpacer(), getBtnAbout());
+    }
+
+    private Node getSpacer() {
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        spacer.setMinWidth(Region.USE_PREF_SIZE);
+        return spacer;
+    }
+
+    private Node getBtnAbout() {
+        Button btnAbout = new Button(null,
+                new ImageView(new Image(
+                        MainWindow.class.getResourceAsStream("AboutDialog.png"), 32, 32, true, true
+                ))
+        );
+        btnAbout.setOnAction(event -> eventBus.post(new AboutEvent(mainStage)));
+        return btnAbout;
     }
 
     public void show() {
         mainStage.show();
     }
-
 
     @Override
     public Parent getRoot() {
