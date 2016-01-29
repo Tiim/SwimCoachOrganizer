bcff6ac7a807c4cb0e9abceb0f3ab6318a9e1faa
Tiim <tim.bachmann@cad.ch>
2016-01-29 13:23:50 +0100

    Remove anoying popup on startup.
    Also resize about image.

--- src/main/java/ch/tiim/sco/update/VersionCheckTask.java
+++ src/main/java/ch/tiim/sco/update/VersionCheckTask.java
@@ -2,11 +2,10 @@ package ch.tiim.sco.update;
 
 
 import com.google.common.eventbus.EventBus;
-import javafx.concurrent.Task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class VersionCheckTask extends Task<Void> {
+public class VersionCheckTask implements Runnable {
     private static final Logger LOGGER = LogManager.getLogger(VersionCheckTask.class.getName());
     private final EventBus eventBus;
 
@@ -16,26 +15,20 @@ public class VersionCheckTask extends Task<Void> {
     }
 
     @Override
-    protected Void call() throws Exception {
-        updateProgress(25, 100);
-        updateMessage("Version: " + VersionChecker.getCurrentVersion());
-        updateProgress(50, 100);
-        updateMessage("New Version" + VersionChecker.getRemoteVersion());
-        LOGGER.info("Version " + VersionChecker.getCurrentVersion() + " --> " +
-                VersionChecker.getRemoteVersion());
-        updateProgress(75, 100);
+    public void run() {
+        LOGGER.info(String.format("Version %s --> %s",
+                VersionChecker.getCurrentVersion(),
+                VersionChecker.getRemoteVersion()));
         if (VersionChecker.isNewVersionAvailable()) {
             try {
-                Thread.sleep(2000); //Wait a bit to not overwhelm the user.
+                Thread.sleep(5000); //Wait a bit to not overwhelm the user.
             } catch (InterruptedException e) {
                 LOGGER.info("Interrupted");
             }
-            updateProgress(100, 100);
             eventBus.post(new NewVersionEvent(
                     VersionChecker.getCurrentVersion(),
                     VersionChecker.getRemoteVersion()
             ));
         }
-        return null;
     }
 }
