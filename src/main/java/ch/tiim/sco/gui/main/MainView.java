cd4b5635dd350f01f3c8293bd362e7dfbf3229c1
Tiim <tim.bachmann@cad.ch>
2016-01-30 11:34:42 +0100

    Add menu to application.

--- src/main/java/ch/tiim/sco/gui/main/MainView.java
+++ src/main/java/ch/tiim/sco/gui/main/MainView.java
@@ -1,22 +1,37 @@
 package ch.tiim.sco.gui.main;
 
 import ch.tiim.sco.gui.View;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
 
 import java.io.InputStream;
 
 
 public abstract class MainView extends View {
 
+    private Menu menu = new Menu("Edit");
+
+    protected static MenuItem createItem(String name, EventHandler<ActionEvent> handler) {
+        MenuItem item = new MenuItem(name);
+        item.setOnAction(handler);
+        return item;
+    }
+
     public void opened() {
-        //Nothing here
+
     }
 
     public InputStream getIcon() {
         return getClass().getResourceAsStream(getClass().getSimpleName() + ".png");
     }
 
+    public Menu getMenu() {
+        return menu;
+    }
+
     public String getName() {
         return getClass().getSimpleName();
     }
-
 }
